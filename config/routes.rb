4fc4613b4c912d6ac0db0d537b10a8a575ab7038
tyler <tylerjerome93@gmail.com>
2017-04-04 19:16:26 -0400

    updated current user to be article creator

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   resources :articles
   get '/signup', to: 'users#new'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users, except: [:new]
 end
