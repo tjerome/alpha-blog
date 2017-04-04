6bac430641f109789921f192b380977ec19225aa
tyler <tylerjerome93@gmail.com>
2017-04-04 19:56:02 -0400

    added admin fuctionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update]
 
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
@@ -48,7 +49,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
     def require_same_user
-      if current_user != @user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only edit your own account"
         redirect_to root_path
       end
