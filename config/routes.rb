e3d12f9df31f079db9054d22df985ea53254134c
tyler <tylerjerome93@gmail.com>
2017-04-07 20:03:03 -0400

    testing and added categories

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
 
   resources :articles
+
   get '/signup', to: 'users#new'
+  resources :users, except: [:new]
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 end
